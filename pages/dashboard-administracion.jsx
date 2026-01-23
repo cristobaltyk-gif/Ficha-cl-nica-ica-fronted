704a89ee8dc59385e10d9615d276d0c97e926075
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 15:32:16 -0300

    Create dashboard-administracion.jsx

---
+++ pages/dashboard-administracion.jsx
@@ -0,0 +1,15 @@
+import "../styles/administracion/dashboard-administracion.css";
+
+export default function DashboardAdministracion() {
+  return (
+    <div className="dashboard dashboard-administracion">
+      <header className="dashboard-header">
+        <h1>Administración</h1>
+      </header>
+
+      <main className="dashboard-body">
+        {/* Configuración estructural */}
+      </main>
+    </div>
+  );
+}
