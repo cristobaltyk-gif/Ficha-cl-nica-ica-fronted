74d6f7f45c900efd9b19ac4dd583f9048d8537f1
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 15:31:27 -0300

    Create dashboard-atencion.jsx

---
+++ pages/dashboard-atencion.jsx
@@ -0,0 +1,15 @@
+import "../styles/atencion/dashboard-atencion.css";
+
+export default function DashboardAtencion() {
+  return (
+    <div className="dashboard dashboard-atencion">
+      <header className="dashboard-header">
+        <h1>Atención Clínica</h1>
+      </header>
+
+      <main className="dashboard-body">
+        {/* Atención en curso */}
+      </main>
+    </div>
+  );
+}
