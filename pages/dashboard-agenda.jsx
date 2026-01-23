693606eb383d2435890c700c1bed236eb90268d9
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 15:30:39 -0300

    Create dashboard-agenda.jsx

---
+++ pages/dashboard-agenda.jsx
@@ -0,0 +1,15 @@
+import "../styles/agenda/dashboard-agenda.css";
+
+export default function DashboardAgenda() {
+  return (
+    <div className="dashboard dashboard-agenda">
+      <header className="dashboard-header">
+        <h1>Agenda</h1>
+      </header>
+
+      <main className="dashboard-body">
+        {/* Agenda.jsx se monta aquí */}
+      </main>
+    </div>
+  );
+}
