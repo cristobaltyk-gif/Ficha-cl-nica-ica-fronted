c93b523da4e0ecd5e3353334686f9287f2669d8a
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 15:31:03 -0300

    Create dashboard-pacientes.jsx

---
+++ pages/dashboard-pacientes.jsx
@@ -0,0 +1,15 @@
+import "../styles/pacientes/dashboard-pacientes.css";
+
+export default function DashboardPacientes() {
+  return (
+    <div className="dashboard dashboard-pacientes">
+      <header className="dashboard-header">
+        <h1>Pacientes</h1>
+      </header>
+
+      <main className="dashboard-body">
+        {/* Registro / búsqueda / listado */}
+      </main>
+    </div>
+  );
+}
