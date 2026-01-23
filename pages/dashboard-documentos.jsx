8cde8b74f5d71713656043291bf36dadc01e1a70
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 15:31:54 -0300

    Create dashboard-documentos.jsx

---
+++ pages/dashboard-documentos.jsx
@@ -0,0 +1,15 @@
+import "../styles/documentos/dashboard-documentos.css";
+
+export default function DashboardDocumentos() {
+  return (
+    <div className="dashboard dashboard-documentos">
+      <header className="dashboard-header">
+        <h1>Documentos</h1>
+      </header>
+
+      <main className="dashboard-body">
+        {/* Órdenes / recetas / certificados */}
+      </main>
+    </div>
+  );
+}
