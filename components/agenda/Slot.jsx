22fe138676d050e25bf8b17c6c65cc8a2d360b6e
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-01-23 08:13:29 -0300

    Update Slot.jsx

--- components/agenda/Slot.jsx
+++ components/agenda/Slot.jsx
@@ -1,6 +1,11 @@
-export default function Slot({ time, status }) {
+export default function Slot({ time, status, onSelect }) {
   return (
-    <div className={`slot slot-${status}`}>
+    <div
+      className={`slot slot-${status}`}
+      onClick={() => onSelect(time)}
+      role="button"
+      tabIndex={0}
+    >
       <span className="slot-time">{time}</span>
     </div>
   );
