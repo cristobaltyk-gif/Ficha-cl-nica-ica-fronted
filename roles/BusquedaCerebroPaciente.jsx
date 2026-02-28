b71e0a923acbcbb593c8242e289a22553b98f874
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-02-28 10:00:17 -0300

    Update BusquedaCerebroPaciente.jsx

--- roles/BusquedaCerebroPaciente.jsx
+++ roles/BusquedaCerebroPaciente.jsx
@@ -99,6 +99,7 @@ export default function BusquedaCerebroPaciente() {
       <h2>Búsqueda de Pacientes</h2>
       {!showForm && (
           <button
+              className="buscar-otro-btn
               onClick={() => {
                   setShowForm(true);
                   setAdmin(null);
