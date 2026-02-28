49bdcd1280e7ccee48e864a813dc736280152dfa
cristobaltyk-gif <cristobal.tyk@gmail.com>
2026-02-28 11:33:38 -0300

    Update AtencionClinicaCerebro.jsx

--- roles/AtencionClinicaCerebro.jsx
+++ roles/AtencionClinicaCerebro.jsx
@@ -364,7 +364,7 @@ async function handleGuardarTodo() {
     }
 
     alert("✅ Evento clínico guardado correctamente");
-    navigate("/agenda", { replace: true, state: { professional: state.professional, date: state.date } });
+    navigate("/medico", { replace: true, state: { professional: state.professional, date: state.date } });
 
   } catch (e) {
     console.error("❌ ERROR GUARDAR EVENTO:", e);
@@ -408,7 +408,7 @@ async function handleModificarEvento() {
     }
 
     alert("✅ Evento clínico modificado correctamente");
-    navigate("/agenda", { replace: true, state: { professional: state.professional, date: state.date } });
+    navigate("/medico", { replace: true, state: { professional: state.professional, date: state.date } });
 
   } catch (e) {
     console.error("❌ ERROR MODIFICAR:", e);
@@ -485,7 +485,7 @@ const edadCalculada = calcularEdad(admin.fecha_nacimiento);
       onGuardar={handleGuardarTodo}
       onModificar={handleModificarEvento}
       onCancelar={() =>
-  navigate("/agenda", {
+  navigate("/medico", {
     replace: true,
     state: {
       professional: state.professional,
